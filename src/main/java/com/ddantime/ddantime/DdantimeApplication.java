792ab760259190ed12f72ae479d84e003942824b
Haneul <jh.neul72@gmail.com>
2025-05-22 12:49:48 +0900

    feat: Spring Boot 서버 기본 셋팅 (#1)

---
+++ src/main/java/com/ddantime/ddantime/DdantimeApplication.java
@@ -0,0 +1,13 @@
+package com.ddantime.ddantime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdantimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdantimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ddantime/ddantime/DdantimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddantime.ddantime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdantimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
