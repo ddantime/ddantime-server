49d57a457e0fa6821ff79c684ab9bac19b5d0381
Haneul <jh.neul72@gmail.com>
2025-06-15 17:23:41 +0900

    test: 사용자 등록 및 조회에 대한 서비스 단위 테스트 작성

--- src/main/java/com/ddantime/ddantime/domain/user/dto/UserCreateRequestDto.java
+++ src/main/java/com/ddantime/ddantime/domain/user/dto/UserCreateRequestDto.java
@@ -1,9 +1,11 @@
 package com.ddantime.ddantime.domain.user.dto;
 
 import com.ddantime.ddantime.domain.user.entity.OsType;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class UserCreateRequestDto {
         private OsType os;
         private String osVersion;
--- src/main/java/com/ddantime/ddantime/domain/user/dto/UserDeviceUpdateRequestDto.java
+++ src/main/java/com/ddantime/ddantime/domain/user/dto/UserDeviceUpdateRequestDto.java
@@ -1,8 +1,10 @@
 package com.ddantime.ddantime.domain.user.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class UserDeviceUpdateRequestDto {
     private String osVersion;
     private String appVersion;
